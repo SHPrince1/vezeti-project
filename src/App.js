06d8be0d04853323304bd815d5dfe648a5784247
Sylvanus Prince <princehsylvanus@gmail.com>
2023-02-10 15:58:39 +0100

    readjustment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Header from "./components/header.tsx";
+import Header from "./components/header.jsx";
 import "./App.css";
 import Footer from "./components/footer.jsx";
 import Cards from "./components/cards.jsx";
