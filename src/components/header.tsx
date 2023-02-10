4c38028aa75779b2c37e2978d08567d7b441e6ae
Sylvanus Prince <princehsylvanus@gmail.com>
2023-02-10 07:39:15 +0100

    text repesenting links added

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import style from "../styles/header.module.css";
-import cart from "../images/cart.jpeg"
+import cart from "../images/cart.jpeg";
 
 const Header = () => {
   return (
@@ -8,14 +8,16 @@ const Header = () => {
       <div className={style.parentBox}>
         <div className={style.logoBox}>
           <h1>VEZETE</h1>
-          {/* <input /> */}
+          <div className={style.links}>
+            <h6>Home |</h6>
+            <h6>Sign up |</h6>
+            <h6>Login</h6>
            
+          </div>
         </div>
-        <div className={style.textBox}> 
-          <img src={cart} alt="images" className={style.imgName}/>
+        <div className={style.textBox}>
+          <img src={cart} alt="images" className={style.imgName} />
         </div>
-
-      
       </div>
     </>
   );
