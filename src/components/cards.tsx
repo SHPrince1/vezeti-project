fa7d818976b0970696bce7d142262441c5621095
Sylvanus Prince <princehsylvanus@gmail.com>
2023-02-10 15:46:36 +0100

    file reformated

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -1,23 +1,22 @@
 import React from "react";
 import style from "../styles/card.module.css";
-import  Modal  from '../components/modal';
+import Modal from "../components/modal";
 const Cards = (props) => {
   return (
     <>
       <div className={style.parentBox}>
         <div className={style.cardBox}>
           <div className={style.imgBox}>
-            <img src={props.img}  alt="product name"/>
+            <img src={props.img} alt="product name" />
           </div>
 
           <div className={style.nameBox}>
-           <p className={style.name}>{props.name}</p>
-
+            <p className={style.name}>{props.name}</p>
           </div>
           <div className={style.priceBox}>
             <p className={style.price}>{props.price}</p>
-         
-          < Modal />
+
+            <Modal />
           </div>
         </div>
       </div>
