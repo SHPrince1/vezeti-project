d1258361d1ea362570ccfc12cd208cbe1d39672d
Sylvanus Prince <princehsylvanus@gmail.com>
2023-07-12 16:59:52 +0100

    netlify adjust

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import {  useDispatch } from "react-redux";
 import style from "../styles/card.module.css";
 // import Modal from "./modal";
 const Cards = (props) => {
